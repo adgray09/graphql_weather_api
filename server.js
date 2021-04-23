3b25d83784e033f3cd485e327ecece5dd1e4895a
adgray09 <adgray29@gmail.com>
2021-04-23 19:31:31 -0400

    made query for grabbing weather based on lat/lon

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const fetch = require('node-fetch');
 const cors = require('cors');
+const fs = require("fs");
+const https = require("https");
+const homedir = require('os').homedir();
+const key = fs.readFileSync(`${homedir}/localhost-key.pem`, 'utf-8');
+const cert = fs.readFileSync(`${homedir}/localhost.pem`, 'utf-8');
 
 
 // graphql schema
@@ -37,49 +42,65 @@ enum Units {
 type Query {
   getWeather(zip: Int!, units: Units): Weather!
   getWeekForecast: weekForecast!
+  getWeatherGeo(lat: Float!, lon: Float!): Weather!
 }
-
 
 `)
 
 const root = {
-    getWeather: async ({ zip, units }) => {
-        const apikey = process.env.OPENWEATHERMAP_API_KEY
-        const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`
-        const res = await fetch(url)
-        const json = await res.json()
+  getWeather: async ({ zip, units }) => {
+    const apikey = process.env.OPENWEATHERMAP_API_KEY
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`
+    const res = await fetch(url)
+    const json = await res.json()
 
-        if (json.cod != '200') {
-            return { temperature: null, description: null, feels_like: null, temp_min: null, temp_max: null, pressure: null, humidity: null, cod: 404, message: "Invalid Zip"}
-        } else {
-            const temperature = json.main.temp
-            const description = json.weather[0].description
-            const feels_like = json.main.feels_like
-            const temp_min = json.main.temp_min
-            const temp_max = json.main.temp_max
-            const pressure = json.main.pressure
-            const humidity = json.main.humidity
-            const cod = json.cod
-            return { temperature, description, feels_like, temp_min, temp_max, pressure, humidity, cod}
-        }
-      },
-      getWeekForecast: async () => {
-        const apikey = process.env.OPENWEATHERMAP_API_KEY
-        const url = `https://api.openweathermap.org/data/2.5/onecall?lat=38.3045&lon=-85.5815&appid=${apikey}&exclude=hourly,minutely&units=imperial`
-        const res = await fetch(url)
-        const json = await res.json()
+    if (json.cod != '200') {
+      return { temperature: null, description: null, feels_like: null, temp_min: null, temp_max: null, pressure: null, humidity: null, cod: 404, message: "Invalid Zip" }
+    } else {
+      const temperature = json.main.temp
+      const description = json.weather[0].description
+      const feels_like = json.main.feels_like
+      const temp_min = json.main.temp_min
+      const temp_max = json.main.temp_max
+      const pressure = json.main.pressure
+      const humidity = json.main.humidity
+      const cod = json.cod
+      return { temperature, description, feels_like, temp_min, temp_max, pressure, humidity, cod }
+    }
+  },
+  getWeekForecast: async () => {
+    const apikey = process.env.OPENWEATHERMAP_API_KEY
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=38.3045&lon=-85.5815&appid=${apikey}&exclude=hourly,minutely&units=imperial`
+    const res = await fetch(url)
+    const json = await res.json()
 
-        const weekForecast = []
+    const weekForecast = []
 
-        for(let i=0; i < json.daily.length; i++) {
-          const grabTemperature = json.daily[i].temp
-          const temperature = grabTemperature.day
-          weekForecast.push(temperature)
-        }
-            
-        return { weekForecast }
-      },
+    for (let i = 0; i < json.daily.length; i++) {
+      const grabTemperature = json.daily[i].temp
+      const temperature = grabTemperature.day
+      weekForecast.push(temperature)
+    }
+
+    return { weekForecast }
+  },
+  getWeatherGeo: async ({ lat, lon }) => {
+    const apikey = process.env.OPENWEATHERMAP_API_KEY
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apikey}`
+    const res = await fetch(url)
+    const json = await res.json()
+
+    const temperature = json.main.temp
+    const description = json.weather[0].description
+    const feels_like = json.main.feels_like
+    const temp_min = json.main.temp_min
+    const temp_max = json.main.temp_max
+    const pressure = json.main.pressure
+    const humidity = json.main.humidity
+    const cod = json.cod
+    return { temperature, description, feels_like, temp_min, temp_max, pressure, humidity }
   }
+}
 
 // create an express app
 const app = express();
@@ -88,13 +109,14 @@ const app = express();
 app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
-    schema,
-    rootValue: root,
-    graphiql: true
-  }));
+  schema,
+  rootValue: root,
+  graphiql: true
+}));
 
 // Start this app
 const port = 4000
-app.listen(port, () => {
-  console.log('Running on port:'+port)
-})
+// app.listen(port, () => {
+//   console.log('Running on port:'+port)
+// })
+https.createServer({ key, cert }, app).listen(port);
